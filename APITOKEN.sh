f7830a18b1414f92b1b0e8fe17810fecc5f89f4b
mmjj <haidersaadi80@gmail.com>
2017-09-03 04:26:17 +0300

    Update APITOKEN.sh

--- APITOKEN.sh
+++ APITOKEN.sh
@@ -1,4 +1,4 @@
-# [BRWUEN TEAM v1.0](https://t.me/vip_api)
+# [ASDFRTQKASPER](https://github.com/botMaysan/API-TOKEN)
  
 
 ◍┋تنصيب سورس بروين ع توكن 
@@ -6,12 +6,9 @@
 ◍┋كيفيه التنصيب تابع ⬇️ : 
 
 ⚙┋افتح ترمنال جديد خلي كود ونتر
-git clone https://github.com/0BRWUEN0/BRWUEN-API-TOKEN.git && cd BRWUEN-API-TOKEN && chmod +x BRWUEN.sh && ./BRWUEN.sh install && ./BRWUEN.sh 
+git clone https://github.com/botMaysan/API-TOKEN.git && cd API-TOKEN&& chmod +x APITOKEN.sh && ./APITOKEN.sh install && ./APITOKEN.sh 
 ⚙┋يكمل تنصيب يطلب منك ID مطور
 ⚙┋تروح فولدر bot تفتح ملف bot
 ⚙┋تخلي توكن سطر 3 ثم ملف config
 ⚙┋ايدي المطور ثم redis-server 
 ⚙┋ورن وبالعافيه اقوة بوت عربي ✔️
-
-🎈┋عدك مشكله بسورس ادخل للكروب
-🎈┋مساعدة https://t.me/joinchat/En3JmEQkezKObPx3NDQ7uA
